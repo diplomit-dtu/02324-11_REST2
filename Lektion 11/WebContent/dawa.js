4dbf743df6810b5c1c16baa904a8720a6b98d8d0
cbudtz <chrbudtz@gmail.com>
2017-03-13 09:43:06 +0100

    Addresse autocomplete

---
+++ Lektion 11/WebContent/dawa.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+
+function loadAddresses(){
+	$.get('http://dawa.aws.dk/vejnavne/autocomplete?q=' + $('input').val(), function(data) {
+		var options = "";
+		$.each(data, function(i, elt) {
+			options +='<option value="'+elt.tekst +'">';
+			$('#datalist').html(options);
+		});
+	
+	})
+}
